376c3a01e4d4eb41483f8fb2f646f88ffd5a94e7
annalisawells <43659312+annalisawells@users.noreply.github.com>
2018-10-05 14:56:24 -0600

    Add files via upload

---
+++ Hogwarts/style.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    var x = document.getElementById("linkList");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
